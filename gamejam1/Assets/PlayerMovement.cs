ed2961a807e7d2418fcf77af16ddae6bbdef9044
Sani Suljanovic <sani.suljanovic@mail.utoronto.ca>
2025-01-11 13:47:17 -0500

    better bus, scrolling road

--- gamejam1/Assets/PlayerMovement.cs
+++ gamejam1/Assets/PlayerMovement.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed;
+    public float moveSpeed;
+
+
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(-moveX, 0, -moveZ);
+
+        transform.Translate(move * moveSpeed * Time.deltaTime);
+    }
 }
